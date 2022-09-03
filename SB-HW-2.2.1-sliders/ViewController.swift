a262c3d45efe9698670fd2a0c3036ec342c31920
LukisMaster <112563277+LukisMaster@users.noreply.github.com>
2022-09-03 21:30:42 +0300

    Initial Commit

---
+++ SB-HW-2.2.1-sliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SB-HW-2.2.1-sliders
+//
+//  Created by Sergey Nestroyniy on 03.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
