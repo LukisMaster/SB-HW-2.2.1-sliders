8621455c4d7c69116e38bd42e3be7eb69677530e
LukisMaster <112563277+LukisMaster@users.noreply.github.com>
2022-09-17 03:18:44 +0300

    Доработана кнопка Done на клавиатуре TextFields. Теперь передача данных из TextFields осуществляется тоже при помощи делегатов

--- SB-HW-2.2.1-sliders/EditViewController.swift
+++ SB-HW-2.2.1-sliders/EditViewController.swift
@@ -41,6 +41,8 @@ class EditViewController: UIViewController {
         colorViewUpdate()
         labelsUpdate()
         textFieldsUpdate()
+        
+        addDoneButtonToKeyboard(textFields: redText, greenText, blueText)
     }
     
     // MARK: Private func
@@ -48,24 +50,70 @@ class EditViewController: UIViewController {
         colorView.backgroundColor = UIColor(red: rgba[0]/255, green: rgba[1]/255, blue: rgba[2]/255, alpha: rgba[3]/255)
     }
     
+    // MARK: Labels Update
     private func labelsUpdate () {
         redLabel.text = String(Int(rgba[0]))
         greenLabel.text = String(Int(rgba[1]))
         blueLabel.text = String(Int(rgba[2]))
     }
     
+    private func labelUpdate (colorTagIndex: Int) {
+        switch colorTagIndex {
+        case 0: redLabel.text = String(Int(rgba[colorTagIndex]))
+        case 1: greenLabel.text = String(Int(rgba[colorTagIndex]))
+        case 2: blueLabel.text = String(Int(rgba[colorTagIndex]))
+        default: fatalError()
+        }
+    }
+    
+    // MARK: Sliders update
     private func slidersUpdate () {
         redSlider.value = Float(rgba[0])
         greenSlider.value = Float(rgba[1])
         blueSlider.value = Float(rgba[2])
     }
- 
+    
+    private func sliderUpdate (colorTagIndex: Int)  {
+        switch colorTagIndex {
+        case 0: redSlider.value = Float(rgba[colorTagIndex])
+        case 1: greenSlider.value = Float(rgba[colorTagIndex])
+        case 2: blueSlider.value = Float(rgba[colorTagIndex])
+        default: fatalError()
+        }
+    }
+    
+    // MARK: Text fields update
     private func textFieldsUpdate () {
         redText.text = String(Int(rgba[0]))
         greenText.text = String(Int(rgba[1]))
         blueText.text = String(Int(rgba[2]))
     }
     
+    private func textFieldUpdate (colorTagIndex: Int) {
+        switch colorTagIndex {
+        case 0: redText.text = String(Int(rgba[colorTagIndex]))
+        case 1: greenText.text = String(Int(rgba[colorTagIndex]))
+        case 2: blueText.text = String(Int(rgba[colorTagIndex]))
+        default: fatalError()
+        }
+    }
+    
+    private func addDoneButtonToKeyboard (textFields: UITextField...) {
+        textFields.forEach { textField in
+            let doneButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(didTapDone))
+            let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+            let toolBarForKeyboard = UIToolbar()
+            toolBarForKeyboard.sizeToFit()
+            toolBarForKeyboard.items = [flexSpace, doneButtonItem]
+            textField.inputAccessoryView = toolBarForKeyboard
+            
+        }
+    }
+    
+    @objc private func didTapDone() {
+        view.endEditing(true)
+    }
+                                                 
     
     
     // MARK: Action func
@@ -76,17 +124,36 @@ class EditViewController: UIViewController {
         labelsUpdate ()
     }
     
-    @IBAction func changeTextField(_ sender: UITextField) {
-        rgba[sender.tag] = CGFloat(Int(sender.text!)!)
-        colorViewUpdate()
-        slidersUpdate()
-        labelsUpdate ()
-    }
-    
     // MARK: Delegate func
     @IBAction func doneButton() {
         delegate?.saveColor(color: rgba)
         dismiss(animated: true)
+    }
+    
+}
+
+extension EditViewController : UITextFieldDelegate {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let colorChannelIndex = textField.tag
+        guard let textColor = textField.text, textColor.count > 0 else {
+            textFieldUpdate(colorTagIndex: colorChannelIndex)
+            return
+        }
+        var uIntColor = UInt(textColor)!
+        if uIntColor > 255 {
+            uIntColor = 255
+            textField.text = "255"
+        }
+        rgba[colorChannelIndex] = CGFloat(uIntColor)
+        colorViewUpdate()
+        sliderUpdate(colorTagIndex: colorChannelIndex)
+        labelUpdate(colorTagIndex: colorChannelIndex)
     }
     
 }
